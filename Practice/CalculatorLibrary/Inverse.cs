e9be1a8ad8937a2c9eb966b1e0490307188985ae
Gaurav Kumar Tiwari <gaurav.tiwari3001@gmail.com>
2022-08-31 11:04:11 +0530

    Added Inverse

---
+++ Practice/CalculatorLibrary/Inverse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CalculatorLibrary
+{
+    public class Inverse : Unary
+    {
+        protected override double Calculate(double[] listOfOperand)
+        {
+            return 1/listOfOperand[0];
+        }
+    }
+}
