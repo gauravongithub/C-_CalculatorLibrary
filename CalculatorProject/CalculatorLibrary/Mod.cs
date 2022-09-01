731006b896843830f0544f8af9b84844e29db019
Gaurav Kumar Tiwari <gaurav.tiwari3001@gmail.com>
2022-09-01 16:40:50 +0530

    Fixed Calculator Function

---
+++ CalculatorProject/CalculatorLibrary/Mod.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CalculatorLibrary
+{
+    public class Mod : Binary
+    {
+        override protected double Calculate(double[] listOfOperands)
+        {
+            double firstOperand = listOfOperands[0];
+            double secondOperand = listOfOperands[1];
+
+            return firstOperand%secondOperand;
+        }
+    }
+}
